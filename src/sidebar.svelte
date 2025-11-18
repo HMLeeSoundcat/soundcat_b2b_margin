10786fc75b23b1207112b9ec9998c86c58f86803
HMLeeSoundcat <hm.lee@soundcat.com>
2025-11-18 20:00:04 +0900

    우선순위 강화

--- src/sidebar.svelte
+++ src/sidebar.svelte
@@ -1,5 +1,5 @@
 <script lang="ts">
-  let {브랜드, 선택된브랜드 = $bindable(), 품목목록가져오기} = $props();
+  let { 브랜드, 선택된브랜드 = $bindable(), 품목목록가져오기 } = $props();
   let 사이드바: HTMLElement | undefined = $state();
 
   let 사이드바열림 = $state(false);
@@ -10,17 +10,21 @@
 </script>
 
 <svelte:window
-  onpointerdown={(e) => {
+  onpointerdown={e => {
     if (!사이드바?.contains(e?.target as HTMLElement)) 사이드바열림 = false;
   }} />
 <div class={["app-sidebars", 사이드바열림 && "opened"]}>
-  <aside class={["app-sidebar"]} bind:this={사이드바}>
+  <aside
+    class={["app-sidebar"]}
+    bind:this={사이드바}>
     <nav>
       {#if 브랜드 && 브랜드.length}
         <ul>
           {#each 브랜드 as 각브랜드}
             <li>
-              <button class={[선택된브랜드 == 각브랜드 && "active"]} onclick={() => (선택된브랜드 = 각브랜드)}>{각브랜드}</button>
+              <button
+                class={[선택된브랜드 == 각브랜드 && "active"]}
+                onclick={() => (선택된브랜드 = 각브랜드)}>{각브랜드}</button>
             </li>
           {/each}
         </ul>
@@ -30,13 +34,18 @@
         <div class="failed">
           <p>브랜드 목록 가져오기를 실패했습니다. 재시도하시겠습니까?</p>
           <p>
-            <button class="retry-btn" onclick={() => 품목목록가져오기()}><i class="fas fa-redo"></i> 재시도</button>
+            <button
+              class="retry-btn"
+              onclick={() => 품목목록가져오기()}><i class="fas fa-redo"></i> 재시도</button>
           </p>
         </div>
       {/if}
     </nav>
   </aside>
-  <button aria-label="브랜드 {사이드바열림 ? '닫기' : '보기'}" class={[사이드바열림 && "opened", "app-sidebar-btn"]} onclick={() => (사이드바열림 = !사이드바열림)}>
+  <button
+    aria-label="브랜드 {사이드바열림 ? '닫기' : '보기'}"
+    class={[사이드바열림 && "opened", "app-sidebar-btn"]}
+    onclick={() => (사이드바열림 = !사이드바열림)}>
     <i class={[`fas fa-angle-double-${사이드바열림 ? "left" : "right"}`]}></i>
   </button>
 </div>
@@ -51,7 +60,7 @@
     align-items: center;
     transition: 0.2s;
     pointer-events: none;
-    z-index: 999;
+    z-index: 999999;
     word-break: keep-all;
     overflow-wrap: break-word;
   }
@@ -96,7 +105,7 @@
     border-color: rgb(10, 127, 251);
   }
   li button:active {
-    background: rgb(9, 111, 220)!important;
+    background: rgb(9, 111, 220) !important;
     color: white;
   }
   li button.active {
